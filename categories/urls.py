f2a866f59fb2d788d514570956180971989a7c77
[[JoseLuisCobo17]] <[[josecobari@alum.us.es]]>
2023-11-28 18:34:36 +0100

    crear, editar y eliminar categoria

--- categories/urls.py
+++ categories/urls.py
@@ -6,4 +6,7 @@ app_name = 'categories'
 urlpatterns = [
     path('<slug:slug>',views.category , name="category"),
     path('', views.categories, name='categories'),
+    path('create/', views.CategoryCreateView, name='categories_create'),
+    path('<slug:slug>/delete',views.CategoryDeleteView , name="categories_delete"),
+    path('<slug:slug>/edit',views.CategoryEditView , name="categories_edit"),
 ]
