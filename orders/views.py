87350cb12f37cb042ef4ba1d2cf5ab8e5d412fae
Joaquin-Arregui <joaquin.arregui2002@gmail.com>
2023-12-07 17:58:26 +0100

    Algunos arreglos

--- orders/views.py
+++ orders/views.py
@@ -10,17 +10,8 @@ from django.utils import timezone
 @login_required(login_url='login')
 def order(request):
     cart = get_or_create_cart(request)
-
-    order = cart.order
-
-    if order is None and request.user.is_authenticated:
-        order = Order.objects.create(cart=cart, user=request.user)
-    if order:
-        request.session['order_id'] = order.order_id
-    print('Este es cart de order', cart)
     return render(request, 'orders/order.html', {
         'cart': cart,
-        'order': order,
         'breadcrumb': breadcrumb()
     })
 
@@ -30,7 +21,9 @@ def envio(request, slug):
     return render(request, 'orders/id_envio.html', context)
 
 def seguimiento(request):
-   return render(request, 'orders/seguimiento.html')
+    orders = Order.objects.filter(user=request.user).order_by('-id')
+    context = {'orders': orders, 'admin': False }
+    return render(request, 'orders/seguimiento.html', context)
 
 def estado(request):
     order_id = request.GET.get('slug')
@@ -51,14 +44,14 @@ def pago(request):
         envio=True
     else:
         envio=False
-    order=Order.objects.create(
-    cart=cart,
-    user=request.user if request.user.is_authenticated else None)
-    context = {'order': order,
-                'pago':pago,
+    context = {'pago':pago,
                 'envio':envio}
     if request.method == 'POST':
-
+        cart.user = None
+        cart.save()
+        order=Order.objects.create(
+            cart=cart,
+            user=request.user if request.user.is_authenticated else None)
         nombre = request.POST.get('firstName')
         apellidos = request.POST.get('lastName')
         calle = request.POST.get('street')
@@ -87,37 +80,6 @@ def pago(request):
         return redirect('/order/envio/' + order.order_id)
     return render(request, 'orders/pasarelapago.html', context)
 
-def pagocontrareembolso(request):
-    id= request.GET.get("cart")
-    cart = get_object_or_404(Cart, pk=id)
-    order=Order.objects.create(
-    cart=cart,
-    user=request.user if request.user.is_authenticated else None)
-    context = {'order': order}
-    if request.method == 'POST':
-
-        nombre = request.POST.get('firstName')
-        apellidos = request.POST.get('lastName')
-        calle = request.POST.get('street')
-        numero = request.POST.get('number')
-        codigopostal = request.POST.get('postalCode')
-        ciudad = request.POST.get('city')    
-
-
-        order.nombre=nombre
-        order.apellidos=apellidos
-        order.calle = calle
-        order.numero = numero
-        order.codigopostal=codigopostal
-        order.ciudad=ciudad
-
-
-        order.save()
-
-
-        return redirect('/order/envio/' + order.order_id)
-    return render(request, 'orders/pagocontrareembolso.html', context)
-
 
 def SeguimientoDeleteView(request):
     order_id = request.GET.get('slug')
@@ -130,8 +92,7 @@ def SeguimientoDeleteView(request):
 def SeguimientoListView(request):
     orders = Order.objects.all().order_by('-id')
     context = {
-        'message': 'Listado de Productos',
-        'orders': orders,
+        'orders': orders, 'admin': True
     }
     return render(request, 'orders/listOrders.html', context)
     #orders = Order.objects.all().order_by('-id')
